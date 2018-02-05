5d87d51194262ff0276fa4e7a9cb5a2f559307df
Paolo <paulstrife88@gmail.com>
2018-02-05 11:11:12 +0900

    add viewport and fix mobile css/js

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,18 +1,25 @@
 // Show and hide the sidebar containeing the search bar and locations list
 function toogleMenu() {
+	var media = window.matchMedia("(min-width: 501px)");
 	var elements = document.getElementsByClassName("hide");
 	if (getComputedStyle(elements[0]).display != "none") {
 		for (var item of elements) {
 			item.style.display = "none";
 		}
-		document.getElementById("menu").style.width = "0%";
+		document.getElementById("menu").style.display = "none";
 		document.getElementById("map").style.width = "100%";
 	}
 	else {
 		for (var item of elements) {
 			item.style.display = "flex";
 		}
-		document.getElementById("menu").style.width = "25%";
-		document.getElementById("map").style.width = "75%";
+		if (media.matched) {
+			document.getElementById("menu").style.display = "block";
+			document.getElementById("map").style.width = "75%";
+		} else {
+			document.getElementById("menu").style.display = "block";
+			document.getElementById("map").style.width = "45%";
+		}
+		
 	}
 }
